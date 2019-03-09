9abad2b5073ab8e6d37175ed0fe4887ad5e46e28
OdessaAlexSV <sav75.odessa@gmail.com>
2019-03-09 21:16:32 +0200

    added a leap year test

---
+++ bash/leap/leap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+usage() {
+	echo "Usage: leap.sh <year>" 
+	exit 1
+}
+
+if  (( "$#" != 1 )); then
+	usage
+fi
+
+if ! [[ $1 =~ ^[0-9]+$ ]]; then
+	usage
+fi
+
+if ((( $1 % 4 == 0 )) && (( $1 % 100 != 0 ))) || (( $1 % 400 ==0 )); then
+   echo 'true'
+else
+   echo 'false'
+fi
--- bash/reverse-string/reverse_string.sh
+++ bash/reverse-string/reverse_string.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#copy=$@
-#len=${#copy}
-#for((i=$len-1;i>=0;i--)); do rev="$rev${copy:$i:1}"; done
-#echo "$rev"
+copy=$1
+len=${#copy}
+for((i=$len-1;i>=0;i--)); do rev="$rev${copy:$i:1}"; done
+echo "$rev"
 
-echo $@ | rev
+#echo $1 | rev
